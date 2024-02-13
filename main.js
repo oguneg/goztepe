d76e3c3156f66f62e2e165b851ed8263af6c7a17
oguneg <oguneg@gmail.com>
2024-02-13 16:29:56 +0300

    Second counter

--- main.js
+++ main.js
@@ -13,14 +13,19 @@ function updateTimeLabels()
     gameData.unixTime = Math.floor(Date.now() / 1000)
     gameData.kskSpan = gameData.unixTime - gameData.kskLastWinUnix
     gameData.altaySpan = gameData.unixTime - gameData.altayLastWinUnix
-    gameData.kskString = (Math.floor(gameData.kskSpan / 86400 / 365)).toString() + " yıl " + (Math.floor(gameData.kskSpan / 86400 % 365)).toString() +" gündür"
-    gameData.altayString = (Math.floor(gameData.altaySpan / 86400 / 365)).toString() + " yıl " + (Math.floor(gameData.altaySpan / 86400 % 365)).toString() +" gündür"
+    
+    //gameData.kskString = (Math.floor(gameData.kskSpan / 86400 / 365)).toString() + " yıl " + (Math.floor(gameData.kskSpan / 86400 % 365)).toString() +" gündür"
+    //gameData.altayString = (Math.floor(gameData.altaySpan / 86400 / 365)).toString() + " yıl " + (Math.floor(gameData.altaySpan / 86400 % 365)).toString() +" gündür"
+
+    gameData.kskString = (Math.floor(gameData.kskSpan / 86400 / 365)).toString() + " yıl " + (Math.floor(gameData.kskSpan / 86400 % 365)).toString() +" gün " + (Math.floor(gameData.kskSpan % 86400 / 3600)).toString() + " saat " + (Math.floor(gameData.kskSpan % 86400 % 3600 / 60)).toString() + " dakika " + (Math.floor(gameData.kskSpan % 60)).toString() + " saniyedir "
+    gameData.altayString = (Math.floor(gameData.altaySpan / 86400 / 365)).toString() + " yıl " + (Math.floor(gameData.altaySpan / 86400 % 365)).toString() +" gün " + (Math.floor(gameData.altaySpan % 86400 / 3600)).toString() + " saat " + (Math.floor(gameData.altaySpan % 86400 % 3600 / 60)).toString() + " dakika " + (Math.floor(gameData.altaySpan % 60)).toString() + " saniyedir "
+
     document.getElementById("kskLabel").innerHTML = "GÖZTEPEMİZ KSKya " + gameData.kskString + " yenilmiyor"
     document.getElementById("altayLabel").innerHTML = "GÖZTEPEMİZ Altaya " + gameData.altayString + " yenilmiyor"
 }
-/*
+
 var mainGameLoop = window.setInterval(function(){
     updateTimeLabels()
 }, 1000)
-*/
+
 window.onload = updateTimeLabels;
